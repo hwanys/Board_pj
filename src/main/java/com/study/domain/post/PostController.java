e54ceb385acde651016f3133eb0bd24a9d72826f
곽승환 <tmdghks7353@gmail.com>
2023-03-21 17:44:32 +0900

    thymeleaf 사용 내용 추가

--- src/main/java/com/study/domain/post/PostController.java
+++ src/main/java/com/study/domain/post/PostController.java
@@ -12,7 +12,14 @@ public class PostController {
     private final PostService postService;
 
     @GetMapping("/post/write.do")
-    public String openPostWrite(Model model){
+    public String openPostWrite(Model model) {
+        String title = "제목",
+                content = "내용",
+                writer = "홍길동";
+
+        model.addAttribute("t", title);
+        model.addAttribute("c", content);
+        model.addAttribute("w", writer);
         return "post/write";
     }
 }
